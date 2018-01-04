35d1ef33174e7209d9b26c0e1ba45d483eb4c52c
Annie Chiang <devbear22@gmail.com>
2018-01-03 18:22:00 -0800

    simplify handling of plusOne for v1

--- src/event/guest.model.js
+++ src/event/guest.model.js
@@ -1,38 +1,40 @@
 const mongoose = require('mongoose');
 
 const guestSchema = mongoose.Schema ({
-  userId: {type: String, required: true},
+  // userId: {type: String, required: true},
   eventId: {type: String, required: true},
   firstName: {type: String, trim: true, required: true},
   lastName: {type: String, trim: true, required: true},
   email: {type: String, trim: true, required: true},
   rsvpStatus: {type: Number,  default: 0}, 
-  plusOne: {
-    needed: {type: Boolean, default: false},
-    number: {type: Number }, //default: 0 ?
-    name: [ 
-      { 
-      firstName: {type: String, trim: true}, //required? 
-      lastName: {type: String, trim: true}, //required?
-      }
-    ]
-  },
+  plusOne: {type: Number},
+  // plusOne: {
+  //   needed: {type: Boolean, default: false},
+  //   number: {type: Number }, //default: 0 ?
+  //   name: [ 
+  //     { 
+  //     firstName: {type: String, trim: true}, //required? 
+  //     lastName: {type: String, trim: true}, //required?
+  //     }
+  //   ]
+  // },
   createdDate: {type: Date, required: true}, 
 });
 
 guestSchema.methods.toClient = function() {
   return {
     id: this._id,
-    userId: this.userId,
+    // userId: this.userId,
     eventId: this.eventId,
     name: `${this.firstName} ${this.lastName}`,
     email: this.email,
     rsvpStatus: this.rsvpStatus,
-    plusOne: {
-      needed: this.needed,
-      number: this.number, //default: 0 ?
-      name: `${this.plusOne.firstName} ${this.plusOne.lastName}`
-      },
+    plusOne: this.plusOne,
+    // plusOne: {
+    //   needed: this.needed,
+    //   number: this.number, //default: 0 ?
+    //   name: `${this.plusOne.firstName} ${this.plusOne.lastName}`
+    //   },
     createdDate: {type: Date, required: true}, 
     }
 }
