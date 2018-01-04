bd6e066df0ca0c3a2ce752073ebb35134625b28d
Annie Chiang <devbear22@gmail.com>
2018-01-04 10:15:36 -0800

    add auth to end points

--- src/event/event.router.js
+++ src/event/event.router.js
@@ -9,44 +9,44 @@ router.use(bodyParser.json());
 // GET ALL EVENTS FROM USER 
 router.get(
   '/',
-  //passport.authenticate('jwt, { session: false }),
+  passport.authenticate('jwt', { session: false }),
   controller.findExistingEvents
 );
 
 // GET ALL ACTIVE EVENTS FROM USER
 router.get(
   '/active',
-  //passport.authenticate('jwt, { session: false }),
+  passport.authenticate('jwt', { session: false }),
   controller.findActiveEvents
 );
 
 // GET ALL PAST EVENTS FROM USER
 router.get(
   '/past',
-  //passport.authenticate('jwt, { session: false }),
+  passport.authenticate('jwt', { session: false }),
   controller.findPastEvents
-)
+);
 
 // GET ALL ARCHIVED EVENTS FROM USER
 router.get(
   '/archived',
-  //passport.authenticate('jwt, { session: false }),
+  passport.authenticate('jwt', { session: false }),
   controller.findArchivedEvents
-)
+);
 
 // CREATE NEW EVENT
 router.post(
   '/',
-  //passport.authenticate('jwt, { session: false }),
+  passport.authenticate('jwt', { session: false }),
   controller.createNewEvent
-)
+);
 
 // MODIFY EXISTING EVENT
 router.put(
   '/:eventId',
-  //passport.authenticate('jwt, { session: false }),
+  passport.authenticate('jwt', { session: false }),
   controller.modifyEventDetails
-)
+);
 
 // PUT - send an array of guest objects
 // first last name email
