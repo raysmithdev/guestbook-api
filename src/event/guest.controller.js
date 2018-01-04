0d53b10f4902537c536c4c82306e4a67ae0f07b9
Annie Chiang <devbear22@gmail.com>
2018-01-03 18:22:23 -0800

    initial commit

---
+++ src/event/guest.controller.js
@@ -0,0 +1,10 @@
+// GET ALL EXISTING GUESTS 
+
+// ADD GUESTS
+
+// REMOVE GUESTS 
+
+// MODIFY GUESTS 
+
+// RSVP status - if still status of 0 or 3, 7 days prior, send reminder email
+// RSVP status - if status of 1, send reminder email 2 days prior ot event 
