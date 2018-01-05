cdd575c79b24033ad99e21d441fee2e50db94b47
Annie Chiang <devbear22@gmail.com>
2018-01-04 17:42:18 -0800

    add one more GET test and 1 POST test

--- src/test/test-event.js
+++ src/test/test-event.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const chatiHttp = require('chai-http');
 const should = chai.should();
 const jwt = require('jsonwebtoken');
-const faker = require('faker');
+// const faker = require('faker');
 
 const mongoose = require('mongoose');
 
@@ -65,6 +65,7 @@ describe('events API', function() {
     console.log('test user->', testUser);
     console.log('test user id->', testUser._id);
     mockJwt = createAuthToken(testUser);
+    console.log('mockJwt ->', mockJwt);
     return seedEventData(testUser._id);
   });
 
@@ -102,6 +103,67 @@ describe('events API', function() {
           res.body.events.should.have.lengthOf(count);
         });
     });
+
+    it('events should return with expected keys', function() {
+      let resEvent;
+      return chai
+        .request(app)
+        .get(`/api/users/${testUser._id}/events`) 
+        .set('Authorization', `Bearer ${mockJwt}`)
+        .then(function(res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.events.should.be.a('array');
+          res.body.events.should.have.lengthOf.at.least(1);
+
+          // check each response for expected keys
+          res.body.events.forEach(function(event) {
+            event.should.be.a('object');
+            event.should.include.keys(expectedKeys);
+          });
+
+          // retrieve single event & check for correct values
+          resEvent = res.body.events[0];
+          return Event.findById(resEvent.id);
+        })
+        .then(function(event) {
+          resEvent.userId.should.equal(event.userId);
+          resEvent.name.should.equal(event.name);
+          resEvent.description.should.equal(event.status);
+          resEvent.locationName.should.equal(event.locationName);
+          resEvent.locationAddress.should.equal(event.locationName);
+          resEvent.startEndTime.should.equal(event.startEndTime);
+        });
+    });
+  
+  // POST endpoint
+  describe('POST endpoint', function() {
+    const expectedKeys = [
+      'id', 'userId', 'name', 'description', 'startDateTime',
+      'locationName', 'locationAddress'
+    ];
+
+    it('create a new event', function() {
+      const newEvent = eventFactory.createOne();
+      return chai
+        .request(app)
+        .post(`/api/users/${testUser._id}/events`)
+        .set('Authorization', `Bearer ${mockJwt}`)
+        .send(newEvent)
+        .then(function(res) {
+          res.should.have.status(201);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.include.keys(expectedKeys);
+          res.body.name.shold.equal(newEvent.name);
+          res.body.description.should.equal(newEvent.description);
+          res.body.locationName.should.equal(newEvent.locationName);
+          res.body.locationAddress.should.equal(newEvent.locationAddress);
+          res.body.status.should.equal(1);
+          res.body.id.should.not.be.null;
+        });
+    });
+  })
   })
 })
 
